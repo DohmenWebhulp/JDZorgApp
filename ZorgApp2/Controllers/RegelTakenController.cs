d971179374fec283df26e82b028c7b2348a86c02
jesseD <jessedohmen1@gmail.com>
2021-08-05 10:09:54 +0200

    commit

--- ZorgApp2/Controllers/RegelTakenController.cs
+++ ZorgApp2/Controllers/RegelTakenController.cs
@@ -15,35 +15,53 @@ namespace ZorgApp2.Controllers
         {
             _context = context;
         }
+        public Klant Klant { get; set; }
         public List<Taak> Taken { get; set; }
-        public List<KSTaak> KSTaken { get; set; }
+        public Taak Taak { get; set; }
         public KSTaak KSTaak { get; set; }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? id)
         {
-            await OphalenTaken();
-            await OphalenKSTaken();
-            return View();
+            Klant = await OphalenKlant(id);
+
+            Taken = await OphalenTaken();
+
+            ViewData["Taken"] = Taken;
+
+            return View(Klant);
         }
 
         //De taken uit de algemene takenlijst.
         //Deze moeten verschijnen in een dropdown op de edit klant pagina en op de bezoekplanningspagina.
-        private async Task OphalenTaken()
+        private async Task<List<Taak>> OphalenTaken()
         {
             Taken = await _context.Taak.ToListAsync();
+            return Taken;
+        }
+
+        private async Task<Klant> OphalenKlant(int? id)
+        {
+            Klant = await _context.Klanten.FirstOrDefaultAsync(u => u.Id == id);
+            return Klant;
         }
 
         //De taken die horen bij een specifieke klant.
         //Deze moeten toegevoegd worden en uiteindelijk ook verschijnen op de edit klant pagina.
-        private async Task OphalenKSTaken()
-        {
-            KSTaken = await _context.KSTaak.ToListAsync();
-        }
 
         [HttpPost]
         public IActionResult ToevoegenKSTaak()
         {
+            KSTaak = new KSTaak();
+            KSTaak.Extra_info = Taak.Extra_info;
+            KSTaak.KlantId = Klant.Id;
             _context.KSTaak.Add(KSTaak);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateKlant()
+        {
+            _context.Klanten.Update(Klant);
             return View();
         }
 
